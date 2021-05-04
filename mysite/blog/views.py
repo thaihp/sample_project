3d5065a2e549c4068849464d32ef2127ae38d86d
Thai Hoang <hoangphanthai@gmail.com>
2021-05-04 21:22:52 +0200

    fix character

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index (request):
     
-    return render(request, "blog/base.html")
-    #return render(request, "blog/main.html")
+    #return render(request, "blog/base.html")
+    return render(request, "blog/main.html")
